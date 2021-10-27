52f20fb53d1c2b4b20ec186a35821aef80916244
wwwsnake <93157112+wwwsnake@users.noreply.github.com>
2021-10-27 21:06:53 +0300

    Add files via upload

---
+++ Converter_to_binary.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.Scanner;
+
+
+
+/**
+ *
+ * @author wwwsn
+ */
+
+
+        
+    /**
+     * @param args the command line arguments
+     */
+public class Program{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+            System.out.print(Converter.toBinary(x));
+    }
+public class  Converter{
+    public toBinary(int x){
+        
+    }
+}
+}    
+
